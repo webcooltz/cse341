5199a927666230025d7495a6e6b3b00f98893a4b
Tyler Tucker <webcooltz@duck.com>
2023-01-02 12:13:26 -0700

    simplified, added nodemon

--- server.js
+++ server.js
@@ -1,38 +1,15 @@
 const express = require('express');
 const http = require('http');
-const debug = require('debug')('node');
+const port = process.env.PORT || 3000;
 
-// routes
+// route declaration
 const index = require('./routes');
 
 var app = express();
 
-const onListening = () => {
-    const addr = server.address();
-    const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-    debug("Listening on " + bind);
-  };
+app.set("port", port);
 
-const onError = error => {
-    if (error.syscall !== "listen") {
-        throw error;
-    }
-    const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-    switch (error.code) {
-        case "EACCES":
-        console.error(bind + " requires elevated privileges");
-        process.exit(1);
-        break;
-        case "EADDRINUSE":
-        console.error(bind + " is already in use");
-        process.exit(1);
-        break;
-        default:
-        throw error;
-    }
-};
-
-// which route it will take depending on the url entered
+// route redirection
 app.use('/', index);
 
 // 404
@@ -41,13 +18,7 @@ app.get('*', (req, res) => {
     res.send('404: oops, bad request!');
   });
 
-const port = 3000;
-app.set("port", port);
-
 const server = http.createServer(app);
-
-server.on("error", onError);
-server.on("listening", onListening);
 
 server.listen(port, function() {
   console.log('API running on localhost: ' + port)
