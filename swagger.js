23bf447821152d76b67527a02a2c13ca0315f3fd
Tyler Tucker <webcooltz@duck.com>
2023-01-28 17:39:19 -0700

    fix swagger cors

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: "Contacts",
         description: "Contacts API for BYUI CSE 341"
     },
-    host: "localhost:3000",
-    schemes: ["http"]
+    host: "cse341-psei.onrender.com",
+    schemes: ["https"]
 };
 
 const outputFile = "./swagger.json";
