87e11e5e0033fa46cc020771587311c2cbe8a510
Tyler Tucker <webcooltz@duck.com>
2023-02-04 17:39:33 -0700

    Revert "Added project for weeks 5-8"
    This reverts commit 8a658cdbed7f3612a5b9c7b324e2becbe5ad9756.

--- rpg/models/character.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const characterSchema = mongoose.Schema({
-    charName: { type: String, required: true },
-    level: { type: Number, required: true },
-    inventory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item', required: true }],
-});
-
-module.exports = mongoose.model('Character', characterSchema);
